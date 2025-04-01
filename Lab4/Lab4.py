c93f8a48419c05fb070fa5aef24dd7ba50242ed4
Vitaliy <Vitaliykretsul13@gmail.com>
2025-04-01 09:53:21 +0300

    Lab4

---
+++ Lab4/Lab4.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value, priority):
+        self.value = value 
+        self.priority = priority 
+        
+
+class PriorityQueue:
+    def __init__(self):
+        self.heap = [] 
+
+    def _heapify_up(self, index):
+        parent = (index - 1) // 2
+        if index > 0 and self.heap[index].priority > self.heap[parent].priority:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self._heapify_up(parent)
+
+    def _heapify_down(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        largest = index
+
+        if left < len(self.heap) and self.heap[left].priority > self.heap[largest].priority:
+            largest = left
+        if right < len(self.heap) and self.heap[right].priority > self.heap[largest].priority:
+            largest = right
+        
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self._heapify_down(largest)
+
+    def insert(self, value, priority):
+        node = Node(value, priority)
+        self.heap.append(node)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_max(self):
+        if not self.heap:
+            return "The queue is empty"
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def peek(self):
+        return self.heap[0] if self.heap else "The queue is empty"
+
+    def display(self):
+        print("A queue with priorities:", self.heap)
+
+
+if __name__ == "__main__":
+    pq = PriorityQueue()
+    pq.insert(50, 2)
+    pq.insert(30, 5)
+    pq.insert(40, 1)
+    pq.insert(2, 4)
+    pq.display()
+    print("The highest priority: ", pq.extract_max())
+    pq.display()
