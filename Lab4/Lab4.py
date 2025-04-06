6bfce56f941c3e1bb86727d48dd1ad9d7aba67b1
Vitaliy <Vitaliykretsul13@gmail.com>
2025-04-06 19:07:29 +0300

    Lab4

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -2,19 +2,38 @@ class Node:
     def __init__(self, value, priority):
         self.value = value 
         self.priority = priority 
-        
+    
+    def __repr__(self):
+        return f"Value: {self.value}, Priority: {self.priority}"
+
 
 class PriorityQueue:
     def __init__(self):
         self.heap = [] 
+        
+    def insert(self, value, priority):
+        node = Node(value, priority)
+        self.heap.append(node)
+        self.heapify_up(len(self.heap) - 1)
 
-    def _heapify_up(self, index):
+    def heapify_up(self, index):
         parent = (index - 1) // 2
         if index > 0 and self.heap[index].priority > self.heap[parent].priority:
             self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
-            self._heapify_up(parent)
+            self.heapify_up(parent)
 
-    def _heapify_down(self, index):
+    def extract_max(self):
+        if not self.heap:
+            return "The queue is empty"
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+        return root
+    
+    def heapify_down(self, index):
         left = 2 * index + 1
         right = 2 * index + 2
         largest = index
@@ -26,23 +45,7 @@ class PriorityQueue:
         
         if largest != index:
             self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
-            self._heapify_down(largest)
-
-    def insert(self, value, priority):
-        node = Node(value, priority)
-        self.heap.append(node)
-        self._heapify_up(len(self.heap) - 1)
-
-    def extract_max(self):
-        if not self.heap:
-            return "The queue is empty"
-        if len(self.heap) == 1:
-            return self.heap.pop()
-
-        root = self.heap[0]
-        self.heap[0] = self.heap.pop()
-        self._heapify_down(0)
-        return root
+            self.heapify_down(largest)
 
     def peek(self):
         return self.heap[0] if self.heap else "The queue is empty"
