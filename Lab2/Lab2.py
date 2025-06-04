6390ffa359201fbdfd6e4d7c6848c52ba2f1544b
Vitaliy <Vitaliykretsul13@gmail.com>
2025-06-04 18:08:15 +0300

    Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -2,13 +2,19 @@ array = list(map(int, input("Enter array: ").split()))
 P = int(input("Enter P: "))
 
 def find_three_sum(array, P):
+        x = 0
         for i in range(len(array)):
                 value = set()  
-                for j in range(i + 1, len(array)): 
+                for j in range(i + 1, len(array)):  
+                        x += 1
                         num_3 = P - array[i] - array[j]
+                        
                         if num_3 in value:
-                                return True
+                                return True, x
+                        
                         value.add(array[j])
+                        
         return False
+
 
 print(find_three_sum(array, P))
