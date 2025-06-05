d2b4a41bffbbf66aa0d29efd0b2fde0aec142097
Vitaliy <Vitaliykretsul13@gmail.com>
2025-06-05 18:40:13 +0300

    Lab8

--- Lab8_test/Test.py
+++ Lab8_test/Test.py
@@ -1,5 +1,6 @@
 import unittest
-from Lab8 import prim_mst
+from Lab8.main import prim_mst
+
 
 class TestPrimAlgorithm(unittest.TestCase):
     def test_small_graph(self):
@@ -8,34 +9,36 @@ class TestPrimAlgorithm(unittest.TestCase):
             [2, 0, 1],
             [3, 1, 0]
         ]
-        self.assertEqual(prim_mst(matrix), 3)
+        total_length, _ = prim_mst(matrix)
+        self.assertEqual(total_length, 3)
 
 
     def test_medium_graph(self):
         matrix = [
-            [0,2,3,0,0],
-            [2,0,1,4,0],
-            [3,1,0,5,0],
-            [0,4,5,0,7],
-            [0,0,0,7,0]
+            [0, 2, 3, 0, 0],
+            [2, 0, 1, 4, 0],
+            [3, 1, 0, 5, 0],
+            [0, 4, 5, 0, 7],
+            [0, 0, 0, 7, 0]
         ]
-        self.assertEqual(prim_mst(matrix), 14)
+        total_length, _ = prim_mst(matrix)
+        self.assertEqual(total_length, 14)
 
 
     def test_disconnected_graph(self):
         matrix = [
-            [0,1,0],
-            [1,0,0],
-            [0,0,0]
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, 0]
         ]
-        self.assertIsNone(prim_mst(matrix))
+        result = prim_mst(matrix)
+        self.assertEqual(result, (None, None))
 
 
     def test_single_node(self):
-        matrix = [
-            [0]
-        ]
-        self.assertEqual(prim_mst(matrix), 0)
+        matrix = [[0]]
+        total_length, _ = prim_mst(matrix)
+        self.assertEqual(total_length, 0)
 
 
 if __name__ == '__main__':
